2897688ec0ead0cf58c7166d74a9c6fdb6dd28e3
keisuke.ueda <ukei2021@gmail.com>
2023-09-06 16:07:37 +0900

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const count = ref(0)
+const addCount = () => {
+    count.value++
+}
+
+</script>
+
+<template>
+    <button @click="addCount">{{ count }} 回クリックしましたよ！</button>
+</template>
+
